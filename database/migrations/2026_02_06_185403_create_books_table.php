34bd1bdbd9105094a83d7421ad14520bce8efe4a
simone sanseverino <simonesanseverino45@gmail.com>
2026-02-06 20:23:46 +0100

    configuration table attributes

--- database/migrations/2026_02_06_185403_create_books_table.php
+++ database/migrations/2026_02_06_185403_create_books_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('title', 100);
+            $table->integer('pages')->nullable();
+            $table->integer('year')->nullable();
             $table->timestamps();
         });
     }
